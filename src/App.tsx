583cb406e616baec353d92e083ea529ceba21358
Sam Kirsch <samdkirsch2@gmail.com>
2020-04-18 20:34:30 -0700

    begin working through debugger setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function handleClick(e: any) {
+    console.log("before the breakpoint");
+    let newVar = e;
+    console.log("after the breakpoint");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +16,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleClick}>Try Me</button>>
       </header>
     </div>
   );
