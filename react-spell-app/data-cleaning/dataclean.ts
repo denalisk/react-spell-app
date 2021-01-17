f21d08cb35c46e0c4e8d60ee58b40f4e38d55494
Sam Kirsch <samdkirsch2@gmail.com>
2021-01-17 15:36:34 -0800

    move git up a directory

---
+++ react-spell-app/data-cleaning/dataclean.ts
@@ -0,0 +1,16 @@
+import Spell from "../src/models/spell.interface";
+
+export function importJsonSpellData() {
+    // Grab file of spells to be imported
+    // Read from json and create objects
+    // map from objects to internal spell schema (maybe field by field?)
+    // 
+}
+
+export function xantharsMapper() {
+    
+}
+
+export function spellDataMapper(sourceField: string, mapper: Function, targetSpell: Spell) {
+
+}
---
+++ react-spell-app/data-cleaning/models/pre-spell.interface.ts
@@ -0,0 +1,16 @@
+export default interface PreSpell {
+    Id: string;
+    Version: string;
+    Name: string;
+    Path: string;
+    Source: string;
+    CastingTime: string;
+    Classes: string;
+    Components: string;
+    Description: string;
+    Duration: string;
+    Level: string;
+    Range: string;
+    Ritual: string;
+    School: string;
+}
